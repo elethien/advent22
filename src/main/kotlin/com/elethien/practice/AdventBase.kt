d18d3b924d7c09e17a4b40e45328a27251d66c68
Ruben Martinez <Ruben.Martinez.M@telefonica.com>
2022-12-01 10:51:58 +0100

    Advent22 DAY01

--- src/main/kotlin/com/elethien/practice/AdventBase.kt
+++ src/main/kotlin/com/elethien/practice/AdventBase.kt
@@ -9,17 +9,16 @@ abstract class AdventBase {
         println("\n=== === =========== === ===")
         println("=== === PROBLEM ${whichTest()} === ===")
         println("=== === =========== === ===")
-        println("Begin Part1 ...")
+        println("\nBegin Part1 ...")
         println(" >>> Part1 training:")
-        println("   [SOL] " + part1(readInput("${whichTest()}-p1-sample")))
+        println("   [SOLUTION] " + run1(readInput("${whichTest()}-p1-sample")))
         println(" >>> Part1 problem:")
-        println("   [SOL] " + part1(readInput("${whichTest()}-p1")))
-
+        println("   [SOLUTION] " + run1(readInput("${whichTest()}-p1")))
         println("\nBegin Part2 ...")
         println(" >>> Part2 training:")
-        println("   [SOL] " + part2(readInput("${whichTest()}-p2-sample")))
+        println("   [SOLUTION] " + run2(readInput("${whichTest()}-p2-sample")))
         println(" >>> Part2 problem:")
-        println("   [SOL] " + part2(readInput("${whichTest()}-p2")))
+        println("   [SOLUTION] " + run2(readInput("${whichTest()}-p2")))
     }
 
     private fun whichTest() = this.javaClass.kotlin.simpleName
@@ -27,22 +26,31 @@ abstract class AdventBase {
     private fun readInput(name: String) : List<String> = kotlin.runCatching {
         File("src/main/resources","${name}.txt").readLines().also{
             if (it.isEmpty())
-                println("  [fileread] Omitting empty file: $name")
+                println("   [fileread] Omitting empty file: $name.txt")
         }
     }.fold(
         { it },
         {
-            println("  [fileread] ERROR READ FILE: $name, cause: ${it.message}")
+            println("   [fileread] ERROR READ FILE: $name.txt, cause: ${it.message} (!!)")
             emptyList()
         }
     )
 
     fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
+    private fun run1(input: List<String>): String =
+        if (input.isEmpty()) " skipped"
+        else part1(input)
+
+    private fun run2(input: List<String>): String =
+        if (input.isEmpty()) "   skipped"
+        else part2(input)
+
     open fun part1(input: List<String>): String{
-        return "NOT IMPLEMENTED(BASE)"
+        return "NOT IMPLEMENTED"
     }
     open fun part2(input: List<String>): String{
-        return "NOT IMPLEMENTED(BASE)"
+        return "NOT IMPLEMENTED"
     }
+
 }
--- src/main/kotlin/com/elethien/practice/D01.kt
+++ src/main/kotlin/com/elethien/practice/D01.kt
@@ -2,9 +2,59 @@ package com.elethien.practice
 
 class D01 : AdventBase() {
     override fun part1(input: List<String>): String {
-        return ""
+        var max = 0
+        var current = 0
+        input.forEach{line ->
+            if(line.trim().isEmpty()) {
+                if (current > max){
+                    max = current
+                }
+                current = 0
+            } else {
+                current += line.trim().toInt()
+            }
+        }
+        // FINAL ITEM
+        if (current > max){
+            max = current
+        }
+        return max.toString()
     }
+
+
     override fun part2(input: List<String>): String {
-        return "    Ommited"
+        var top3 = mutableListOf(0,0,0) // SORTED Big to low
+        var current = 0
+        input.forEach{line ->
+            if(line.trim().isEmpty()) {
+                if (current > getMin(top3)){
+                    addToTop3(top3,current)
+                }
+                current = 0
+            } else {
+                current += line.trim().toInt()
+            }
+        }
+        //final ITEM
+        if (current > getMin(top3)){
+            addToTop3(top3,current)
+        }
+        return top3.sum().toString()
+    }
+
+    private fun getMin(top3: List<Int>): Int = top3[2]
+
+
+    private fun addToTop3(top3: MutableList<Int>, current: Int) {
+        var added = false
+        var pos = 0
+        while (! added && pos < top3.size) {
+            if (current > top3[pos]){
+                top3.add(pos, current)
+                added = true
+            }
+            pos++
+        }
+        top3.removeAt(3)
     }
 }
