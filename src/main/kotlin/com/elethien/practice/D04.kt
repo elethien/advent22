6092cf85d3bd87f19b4035187cadfd5d0e797dac
Ruben Martinez <Ruben.Martinez.M@telefonica.com>
2022-12-04 10:09:23 +0100

    Advent22 Day04

--- src/main/kotlin/com/elethien/practice/D04.kt
+++ src/main/kotlin/com/elethien/practice/D04.kt
@@ -3,9 +3,12 @@ class D04 : AdventBase() {
     override fun part1(input: List<String>): String {
         var total = 0
         var lines = 0
-        input.forEach { line ->
+        input.forEachIndexed { i, line ->
             if (!line.trim().isEmpty()) {
-
+                val elfs = line.split(',')
+                if (insideAofB(elfs[0], elfs[1]) || insideAofB(elfs[1], elfs[0]) ){
+                    total ++
+                }
             }
         }
         return total.toString()
@@ -15,11 +18,34 @@ class D04 : AdventBase() {
         var total = 0
         var num = 0
         var common = emptySet<Char>()
-        input.forEach { line ->
+        input.forEachIndexed { i, line ->
             if (!line.trim().isEmpty()) {
-                num++
+                val elfs = line.split(',')
+                if (overlap(elfs[0], elfs[1])  ){
+                    total ++
+                }
             }
         }
         return total.toString()
+    }
+
+    private fun insideAofB(elfA: String, elfB: String): Boolean {
+        val startA = elfA.split('-')[0].toInt()
+        val endA = elfA.split('-')[1].toInt()
+
+        val startB = elfB.split('-')[0].toInt()
+        val endB = elfB.split('-')[1].toInt()
+
+        return (startA >= startB) && (endA <= endB)
+    }
+
+    private fun overlap(elfA: String, elfB: String): Boolean {
+        val startA = elfA.split('-')[0].toInt()
+        val endA = elfA.split('-')[1].toInt()
+
+        val startB = elfB.split('-')[0].toInt()
+        val endB = elfB.split('-')[1].toInt()
+
+        return (startA <= endB) && (endA >= startB)
     }
 }
---
+++ src/main/kotlin/com/elethien/practice/D05.kt
@@ -0,0 +1,29 @@
+package com.elethien.practice
+class D05 : AdventBase() {
+    override fun part1(input: List<String>): String {
+        var total = 0
+        var lines = 0
+        input.forEachIndexed { i, line ->
+            if (!line.trim().isEmpty()) {
+                val elfs = line.split(',')
+
+            }
+        }
+        return total.toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        var total = 0
+        var num = 0
+        var common = emptySet<Char>()
+        input.forEachIndexed { i, line ->
+            if (!line.trim().isEmpty()) {
+                val elfs = line.split(',')
+
+            }
+        }
+        return total.toString()
+    }
+
+
+}
